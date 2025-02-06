a5ad5f7e3f9c7819b90a74b031a257ca121a95af
Md. Saiful Islam <saiful.cu.19@gmail.com>
2025-02-06 16:45:53 +0600

    Done Task

--- src/test/java/org/example/Main.java
+++ src/test/java/org/example/Main.java
@@ -9,7 +9,14 @@ public class Main {
         System.setProperty("webDriver.chrome.driver","src/main/resources/chromedriver");
         WebDriver driver = new ChromeDriver() ;
         driver.get("https://www.google.com/");
-
-        Assert.assertEquals(driver.getTitle(),"Google","Title isn't match");
+        String Title = driver.getTitle();
+        System.out.println(Title);
+        if(Title.equals("Google")){
+            System.out.println("Title Match");
+        }else {
+            System.out.println("Title MisMatch");
+        }
+        Assert.assertEquals(Title,"Google","Title isn't match");
+        driver.quit();
     }
 }
